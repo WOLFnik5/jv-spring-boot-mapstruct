a5884e816777cd810e7c8db27ee79bc6b0f6462f
MYKOLA YAKYMCHUK <m.yakymchuk5@gmail.com>
2025-12-07 02:06:27 +0100

    Implement Mappers

--- src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/GroupMapper.java
@@ -3,7 +3,10 @@ package mate.academy.mapstruct.mapper;
 import mate.academy.mapstruct.dto.group.CreateGroupRequestDto;
 import mate.academy.mapstruct.dto.group.GroupDto;
 import mate.academy.mapstruct.model.Group;
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValueCheckStrategy;
 
+@Mapper(componentModel = "spring", nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
 public interface GroupMapper {
     GroupDto toDto(Group group);
 
--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -1,14 +1,57 @@
 package mate.academy.mapstruct.mapper;
 
+import java.util.stream.Collectors;
 import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
+import mate.academy.mapstruct.model.Group;
 import mate.academy.mapstruct.model.Student;
+import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.AfterMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValueCheckStrategy;
 
+@Mapper(componentModel = "spring", nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
 public interface StudentMapper {
+
+    @Mapping(target = "groupId", source = "group.id")
+    @Mapping(target = "subjectIds", ignore = true)
     StudentDto toDto(Student student);
 
+    @AfterMapping
+    default void setSubjectIds(@MappingTarget StudentDto studentDto, Student student) {
+        if (student.getSubjects() != null) {
+            studentDto.setSubjectIds(
+                    student.getSubjects().stream()
+                            .map(Subject::getId)
+                            .collect(Collectors.toList())
+            );
+        }
+    }
+
+    @Mapping(target = "groupId", source = "group.id")
     StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "socialSecurityNumber", ignore = true)
+    @Mapping(target = "group", ignore = true)
+    @Mapping(target = "subjects", ignore = true)
     Student toModel(CreateStudentRequestDto requestDto);
+
+    @AfterMapping
+    default void setGroupAndSubjects(@MappingTarget Student student,
+                                     CreateStudentRequestDto requestDto) {
+        if (requestDto.groupId() != null) {
+            student.setGroup(new Group(requestDto.groupId()));
+        }
+        if (requestDto.subjects() != null) {
+            student.setSubjects(
+                    requestDto.subjects().stream()
+                            .map(Subject::new)
+                            .collect(Collectors.toList())
+            );
+        }
+    }
 }
